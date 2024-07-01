65c86ca404998cf3627c1937dda732f9778a8fbe
Rafael Medeiros <mederafael@gmail.com>
2024-06-30 21:36:28 -0300

    versão final

--- kafka-cluster/kafka/docker-entrypoint.sh
+++ kafka-cluster/kafka/docker-entrypoint.sh
@@ -2,62 +2,43 @@
 set -e
 
 # Corrige permissões dos volumes
-chown -R kafka:kafka /var/lib/kafka /opt/kafka/config
-
-#auto.leader.rebalance.enable=true
-#min.insync.replicas=2
-#export KAFKA_JVM_PERFORMANCE_OPTS="-server -XX:+UseG1GC -XX:MaxGCPauseMillis=20 -XX:InitiatingHeapOccupancyPercent=35 -XX:+DisableExplicitGC -Djava.awt.headless=true"
+chown -R kafka:kafka /var/lib/kafka /opt/kafka/config /var/log/kafka/
 
 
 echo "broker.id=${KAFKA_BROKER_ID}
 listeners=${KAFKA_LISTENERS}
 advertised.listeners=${KAFKA_ADVERTISED_LISTENERS}
 zookeeper.connect=${KAFKA_ZOOKEEPER_CONNECT}
-log.dirs=${KAFKA_LOG_DIRS}
-num.partitions=1
-default.replication.factor=1
-min.insync.replicas=1
+log.dirs=${KAFKA_LOG_DIRS:-/var/log/kafka}
+num.partitions=3
+default.replication.factor=3
+min.insync.replicas=2
 message.max.bytes=1000012
-request.timeout.ms=30000" >> "/opt/kafka/config/server2.properties"
+auto.leader.rebalance.enable=true
+request.timeout.ms=30000" > "/opt/kafka/config/server2.properties"
 
 
-# Define os argumentos padrão do Kafka
 KAFKA_CONFIG="/opt/kafka/config/server2.properties"
 
-# Espera o Kafka iniciar
-while ! ping -c 1 "kafka1"; do
-  echo "Waiting for the host to be reachable..."
-  sleep 5
-done
-
-# Espera o Kafka iniciar
-while ! ping -c 1 "kafka2"; do
-  echo "Waiting for the host to be reachable..."
-  sleep 5
-done
-
-# Espera o Kafka iniciar
-while ! ping -c 1 "kafka3"; do
-  echo "Waiting for the host to be reachable..."
-  sleep 5
-done
-
-# Inicia o Kafka
-su-exec kafka kafka-server-start.sh $KAFKA_CONFIG &
-
-#Função para criar tópico se não existir
- create_topic() {
-  TOPIC_NAME="credit-card-transactions"
-  if /opt/kafka/bin/kafka-topics.sh --list --bootstrap-server ${KAFKA_BROKER_HOST_NAME} | grep -q $TOPIC_NAME; then
-    echo "Tópico '$TOPIC_NAME' já existe."
-  else
-    /opt/kafka/bin/kafka-topics.sh --create --topic $TOPIC_NAME --bootstrap-server ${KAFKA_BROKER_HOST_NAME} --partitions 3 --replication-factor 3
-    echo "Tópico '$TOPIC_NAME' criado com sucesso."
-  fi
+# Função para verificar se um contêiner Kafka está pronto
+check_container_ready() {
+  local container_name=$1
+  while ! ping -c 1 $container_name &>/dev/null; do
+    echo "Aguardando contêiner Kafka $container_name ficar pronto..."
+    sleep 5
+  done
+  return 0
 }
 
-# Criação de tópicos de transações bancárias
-create_topic "credit-card-transactions"
-
-# Mantém o contêiner ativo
-wait
+# Loop para verificar todos os contêineres zookeeper2 e kafka
+while true; do
+  if check_container_ready zookeeper1 && check_container_ready zookeeper2 && check_container_ready zookeeper3 && check_container_ready kafka1 && check_container_ready kafka2 && check_container_ready kafka3; then
+    # Iniciar o Kafka
+    echo "Iniciando o Kafka..."
+    su-exec kafka kafka-server-start.sh $KAFKA_CONFIG
+    break
+  else
+    echo "Nem todos os contêineres Kafka estão prontos. Tentando novamente em 5 segundos..."
+    sleep 5
+  fi
+done
--- kafka-cluster/zookeeper/docker-entrypoint.sh
+++ kafka-cluster/zookeeper/docker-entrypoint.sh
@@ -8,39 +8,38 @@ syncLimit=5\n\
 dataDir=/tmp/zookeeper\n\
 dataLogDir=/var/log/zookeeper\n\
 autopurge.snapRetainCount=3\n\
-autopurge.purgeInterval=1" >> /opt/zookeeper/conf/zoo.cfg
-
-# Configura o clientPort no zoo.cfg
-if [ -n "$ZOO_CLIENT_PORT" ]; then
-  echo "clientPort=$ZOO_CLIENT_PORT" >> /opt/zookeeper/conf/zoo.cfg
-  echo "$ZOO_SERVER1" >> /opt/zookeeper/conf/zoo.cfg
-  echo "$ZOO_SERVER2" >> /opt/zookeeper/conf/zoo.cfg
-  echo "$ZOO_SERVER3" >> /opt/zookeeper/conf/zoo.cfg
-fi
-
-cat /opt/zookeeper/conf/zoo.cfg
+autopurge.purgeInterval=1\n\
+clientPort=$ZOO_CLIENT_PORT\n\
+$ZOO_SERVER1\n\
+$ZOO_SERVER2\n\
+$ZOO_SERVER3" > /opt/zookeeper/conf/zoo.cfg
 
 # Criar o arquivo myid
 mkdir -p /tmp/zookeeper
 echo ${ZOO_MY_ID} > /tmp/zookeeper/myid
+# Função para verificar se um contêiner Kafka está pronto
+check_container_ready() {
+  local container_name=$1
+  while ! ping -c 1 $container_name &>/dev/null; do
+    echo "Aguardando contêiner zookeeper $container_name ficar pronto..."
+    sleep 5
+  done
+  return 0
+}
 
-while ! ping -c 1 "zookeeper3"; do
-  echo "Waiting for the host to be reachable..."
-   sleep 5
+# Loop para verificar todos os contêineres zookeeper
+while true; do
+  if check_container_ready zookeeper1 && check_container_ready zookeeper2 && check_container_ready zookeeper3; then
+    # Iniciar o Kafka
+    echo "Iniciando o zookeeper..."o
+    exec zkServer.sh start-foreground
+    break
+  else
+    echo "Nem todos os contêineres zookeeper estão prontos. Tentando novamente em 5 segundos..."
+    sleep 5
+  fi
 done
 
- while ! ping -c 1 "zookeeper2"; do
-   echo "Waiting for the host to be reachable..."
-   sleep 5
- done
-
- while ! ping -c 1 "zookeeper1"; do
-   echo "Waiting for the host to be reachable..."
-   sleep 5
- done
-
-# Executa o ZooKeeper padrão
-exec zkServer.sh start-foreground
 
 # Permite executar comandos arbitrários, se fornecidos
 # if [ "${1:0:1}" = '-' ]; then
